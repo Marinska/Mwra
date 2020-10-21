185ed07137c1cf763385e17e68bcb13d7fa18325
Umar Ibnu Zainal Muttaqin <43942718+Marinska@users.noreply.github.com>
2020-10-21 22:30:36 +0700

    Create mwra3.cpp

---
+++ mwra3.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int luas,panjang,lebar; //ini kalo di pseudocode dibuat untuk perkenalan variable,
+	cout<<"Masukan nilai panjang = "; 
+	cin>>panjang; //disini buat perintah input
+	cout<<"Masukan nilai lebar = ";
+	cin>>lebar; //disni juga perintah input
+	
+	luas = panjang*lebar; //disini proses algoritmanya, dimana program disuruh menjalankan aritmatika untuk menghitung nilai luas
+	
+	cout<<"Luas persegi panjang adalah "<<luas<<" cm";	//ini buat nampilin hasil perhitungan program
+}
+
+
+//pseudocode program diatas
+
+//var luas,panjang,lebar;
+
+//input nilai panjang
+//input nilai lebar
+
+//hitung nilai luas dengan perkalian antara panjang dan lebar
+
+//tulis hasil nilai dari luas
